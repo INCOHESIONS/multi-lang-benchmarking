18d65c9bd574e5faf9eca365f0c4ca4bccb6e068
iNCOHESiONS <incohesions@gmail.com>
2025-12-09 20:52:07 -0300

    improve performance

--- Python - GPU, compushady/main.py
+++ Python - GPU, compushady/main.py
@@ -11,11 +11,9 @@ from sys import argv
 from time import perf_counter_ns
 
 import compushady
-from compushady.formats import B8G8R8A8_UNORM, R32G32_UINT
+from compushady.formats import R8_UNORM, R32G32_UINT
 from compushady.shaders.hlsl import compile
 from PIL import Image  # type: ignore
-
-compushady.config.set_debug(True)
 
 chars = ascii_letters + digits
 
@@ -41,7 +39,9 @@ save_image = argv[4] == "true"
 SHADER = f"""
     #define INFINITY 3.402823466e+38F
 
-    RWTexture2D<float4> target;
+    static float INV_255 = 1.0f / 255.0F;
+
+    RWTexture2D<float> target;
     Buffer<uint2> points;
 
     [numthreads(8, 8, 1)]
@@ -49,19 +49,17 @@ SHADER = f"""
     {{
         float minDist = INFINITY;
 
-        for (uint i = 0; i < {number_of_points}; i++)
+        for (uint i = 0; i < {number_of_points}u; i++)
             minDist = min(minDist, dot(points[i] - tid.xy, points[i] - tid.xy));
 
-        const float color = 1.0F - clamp(sqrt(minDist), 0.0F, 255.0F) / 255.0F;
-
-        target[tid.xy] = float4(color, color, color, 1.0F);
+        target[tid.xy] = 1.0F - clamp(sqrt(minDist), 0.0F, 255.0F) * INV_255;
     }}
 """
 
 texture_width = round_to_nearest_larger_power_of_2(width)
 texture_height = round_to_nearest_larger_power_of_2(height)
 
-target = compushady.Texture2D(texture_width, texture_height, format=B8G8R8A8_UNORM)
+target = compushady.Texture2D(texture_width, texture_height, format=R8_UNORM)
 
 staging = compushady.Buffer(
     number_of_points * 2 * sizeof(c_int), compushady.HEAP_UPLOAD
@@ -96,12 +94,10 @@ readback = compushady.Buffer(target.size, compushady.HEAP_READBACK)
 target.copy_to(readback)
 
 image = Image.frombuffer(
-    "RGBA",
+    "L",
     (texture_width, texture_height),
     readback.readback(),  # pyright: ignore[reportArgumentType]
-)
-
-image = image.crop((0, 0, width, height))
+).crop((0, 0, width, height))
 
 if not os.path.exists("img/"):
     os.mkdir("img/")
